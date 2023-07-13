50c0b7cc0e1b64b0fdfd83c94461b0524ceff22b
LucasPereyra <lucaspereyra2019@gmail.com>
2023-07-12 23:26:00 -0300

    Preentrega fixed

--- main.js
+++ main.js
@@ -10,11 +10,18 @@ bienvenida ()
 
 function elegir(){
     let Opciones = prompt("Que servicio desea contratar? masajes o uñas")
-    
-    while(Opciones !== "masajes"){
 
+    while (Opciones !== "masajes" && Opciones !== "uñas"){
+        alert ("Elija una opción válida")
+        Opciones = prompt("Que servicio desea contratar? masajes o uñas")
     }
+    if (Opciones === "masajes"){
+        alert ("Nos contactaremos para fijar un turno")
     }
+    else {
+        alert ("Uñas está libre, acercate al local")
+    }
+}
 
 
 
