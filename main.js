2dd95a2706a2a201cae44c5022d0423f0e044b9b
LucasPereyra <lucaspereyra2019@gmail.com>
2023-07-12 21:51:32 -0300

    Primera entrega JS

---
+++ main.js
@@ -0,0 +1,27 @@
+function bienvenida () {
+    let nombre = prompt("¿Cómo deberíamos llamarlo?")
+    alert("Bienvenido " + nombre)
+
+}
+
+bienvenida ()
+
+let
+
+function elegir(){
+    let Opciones = prompt("Que servicio desea contratar? masajes o uñas")
+    
+
+    
+    if(elegir = masajes){
+        return prompt("Nos contactaremos para agendar un turno")    }
+        else{
+            prompt("Podes acercar al local para hacerte las uñas")
+        }
+        while(Opciones != "masajes" || "uñas"){
+            alert("Elija una de las dos opciones validas")
+}
+}
+
+
+elegir ()
